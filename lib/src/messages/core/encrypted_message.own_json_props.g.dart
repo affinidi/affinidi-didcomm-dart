f0cd37ad3dfec73b006267fc2ba8b94f913694fd
Ivan Branets <ivan.b@affinidi.com>
2025-05-15 14:56:58 +0200

    chore: improved serialization

---
+++ lib/src/messages/core/encrypted_message.own_json_props.g.dart
@@ -0,0 +1,18 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'encrypted_message.dart';
+
+// **************************************************************************
+// OwnJsonPropertiesGenerator
+// **************************************************************************
+
+extension EncryptedMessageOwnPropertiesExtension on EncryptedMessage {
+  List<String> ownProperties() => [
+    'ciphertext',
+    'protected',
+    'recipients',
+    'tag',
+    'iv',
+  ];
+}
---
+++ lib/src/messages/core/signed_message.own_json_props.g.dart
@@ -0,0 +1,12 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signed_message.dart';
+
+// **************************************************************************
+// OwnJsonPropertiesGenerator
+// **************************************************************************
+
+extension SignedMessageOwnPropertiesExtension on SignedMessage {
+  List<String> ownProperties() => ['payload', 'signatures'];
+}
