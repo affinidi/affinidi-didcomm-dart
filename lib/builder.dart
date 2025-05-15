2f84b9524d2281883f57e6b3b53e5de3d76da301
Ivan Branets <ivan.b@affinidi.com>
2025-05-15 13:41:07 +0200

    chore: filter own json props

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,6 +1,6 @@
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
-import './src/generators/own_properties_generator.dart';
+import 'src/generators/own_json_properties_generator.dart';
 
-Builder ownPropertiesBuilder(BuilderOptions options) =>
-    PartBuilder([OwnPropertiesGenerator()], '.own_props.g.dart');
+Builder ownJsonPropertiesBuilder(BuilderOptions options) =>
+    PartBuilder([OwnJsonPropertiesGenerator()], '.own_json_props.g.dart');
---
+++ lib/src/annotations/own_json_properties.dart
@@ -0,0 +1,3 @@
+class OwnJsonProperties {
+  const OwnJsonProperties();
+}
