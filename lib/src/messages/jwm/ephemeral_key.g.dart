9ea758c4521cfbfc63b0dc34b2c9a82acdf959eb
Ivan Branets <ivan.b@affinidi.com>
2025-05-21 17:01:27 +0200

    chore: big int to bytes extension

--- lib/src/messages/jwm/ephemeral_key.g.dart
+++ lib/src/messages/jwm/ephemeral_key.g.dart
@@ -9,16 +9,22 @@ part of 'ephemeral_key.dart';
 EphemeralKey _$EphemeralKeyFromJson(Map<String, dynamic> json) => EphemeralKey(
   keyType: $enumDecode(_$EphemeralKeyTypeEnumMap, json['kty']),
   curve: $enumDecode(_$CurveTypeEnumMap, json['crv']),
-  x: json['x'] as String,
-  y: json['y'] as String?,
+  x: const Base64UrlConverter().fromJson(json['x'] as String),
+  y: _$JsonConverterFromJson<String, Uint8List>(
+    json['y'],
+    const Base64UrlConverter().fromJson,
+  ),
 );
 
 Map<String, dynamic> _$EphemeralKeyToJson(EphemeralKey instance) =>
     <String, dynamic>{
       'kty': _$EphemeralKeyTypeEnumMap[instance.keyType]!,
       'crv': _$CurveTypeEnumMap[instance.curve]!,
-      'x': instance.x,
-      'y': instance.y,
+      'x': const Base64UrlConverter().toJson(instance.x),
+      'y': _$JsonConverterToJson<String, Uint8List>(
+        instance.y,
+        const Base64UrlConverter().toJson,
+      ),
     };
 
 const _$EphemeralKeyTypeEnumMap = {
@@ -31,3 +37,13 @@ const _$CurveTypeEnumMap = {
   CurveType.secp256k1: 'secp256k1',
   CurveType.x25519: 'X25519',
 };
+
+Value? _$JsonConverterFromJson<Json, Value>(
+  Object? json,
+  Value? Function(Json json) fromJson,
+) => json == null ? null : fromJson(json as Json);
+
+Json? _$JsonConverterToJson<Json, Value>(
+  Value? value,
+  Json? Function(Value value) toJson,
+) => value == null ? null : toJson(value);
