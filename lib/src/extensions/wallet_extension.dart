c270ac19f006d852961c9de0ba914ed04322f7c4
Ivan Branets <ivan.b@affinidi.com>
2025-05-22 23:41:16 +0200

    chore: add curve casting for ecdh

--- lib/src/extensions/wallet_extension.dart
+++ lib/src/extensions/wallet_extension.dart
@@ -2,15 +2,23 @@ import 'dart:typed_data';
 
 import 'package:ssi/ssi.dart';
 
+import '../../didcomm.dart';
+
 extension WalletExtension on Wallet {
   Future<Uint8List> computeEcdhSecret({
     required String keyId,
     required Uint8List othersPublicKeyBytes,
   }) async {
-    final keyPair = (await getKeyPair(keyId)) as P256KeyPair;
+    final keyPair = await getKeyPair(keyId);
 
-    return await keyPair.computeEcdhSecret(
-      Uint8List.fromList(othersPublicKeyBytes),
-    );
+    if (keyPair is P256KeyPair) {
+      return await keyPair.computeEcdhSecret(othersPublicKeyBytes);
+    }
+
+    if (keyPair is Ed25519KeyPair) {
+      return await keyPair.computeEcdhSecret(othersPublicKeyBytes);
+    }
+
+    throw UnsupportedKeyTypeError(keyPair.publicKey.type);
   }
 }
