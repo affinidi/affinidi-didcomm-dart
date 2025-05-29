b57274b9f12f682d174c3ba99c875ee00bd68584
Ivan Branets <ivan.b@affinidi.com>
2025-05-29 19:06:58 +0200

    chore: fetch authentication tokens

---
+++ lib/src/common/authentication_tokens/authentication_tokens.dart
@@ -0,0 +1,33 @@
+import 'package:didcomm/src/converters/epoch_seconds_converter.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'authentication_tokens.g.dart';
+
+@JsonSerializable()
+class AuthenticationTokens {
+  @JsonKey(name: 'access_token')
+  final String accessToken;
+
+  @JsonKey(name: 'access_expires_at')
+  @EpochSecondsConverter()
+  final DateTime accessExpiresAt;
+
+  @JsonKey(name: 'refresh_token')
+  final String refreshToken;
+
+  @JsonKey(name: 'refresh_expires_at')
+  @EpochSecondsConverter()
+  final DateTime refreshExpiresAt;
+
+  AuthenticationTokens({
+    required this.accessToken,
+    required this.accessExpiresAt,
+    required this.refreshToken,
+    required this.refreshExpiresAt,
+  });
+
+  factory AuthenticationTokens.fromJson(Map<String, dynamic> json) =>
+      _$AuthenticationTokensFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthenticationTokensToJson(this);
+}
