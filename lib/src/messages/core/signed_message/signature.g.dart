f10373c7bcc57f08da76078f55a7dadfb70a523a
Ivan Branets <ivan.b@affinidi.com>
2025-05-30 10:14:09 +0200

    fix: signature

--- lib/src/messages/core/signed_message/signature.g.dart
+++ lib/src/messages/core/signed_message/signature.g.dart
@@ -7,14 +7,15 @@ part of 'signature.dart';
 // **************************************************************************
 
 Signature _$SignatureFromJson(Map<String, dynamic> json) => Signature(
-      protected: JwsHeader.fromJson(json['protected'] as Map<String, dynamic>),
+      protected:
+          const JwsHeaderConverter().fromJson(json['protected'] as String),
       signature:
           const Base64UrlConverter().fromJson(json['signature'] as String),
       header: SignatureHeader.fromJson(json['header'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$SignatureToJson(Signature instance) => <String, dynamic>{
-      'protected': instance.protected,
+      'protected': const JwsHeaderConverter().toJson(instance.protected),
       'signature': const Base64UrlConverter().toJson(instance.signature),
       'header': instance.header,
     };
