40de5bcca4cc81e59efc6645d843b1ff68b01e8e
Ivan Branets <ivan.b@affinidi.com>
2025-06-12 10:55:11 +0200

    chore: add todos for tests

---
+++ test/encrypted_message_with_bip32_ed25519_wallet_test.dart
@@ -0,0 +1,3 @@
+// TODO: add only succsful test cases for encrypted message with BIP32 Ed25519 wallet
+// only "Pack and unpack encrypted message successfully" from test/encrypted_message_with_persistent_test.dart
+void main() {}
---
+++ test/singned_message_test.dart
@@ -0,0 +1,3 @@
+// TODO: tests for SignedMessage. There is no difference for persistent and getX25519PublicKey wallets,
+// so P curves and Edward curves can be tested together
+void main() {}
