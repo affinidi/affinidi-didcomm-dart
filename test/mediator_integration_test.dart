9bd5c9d1a689474a33d12fcbc9e082f871c08fc8
Ivan Branets <ivan.b@affinidi.com>
2025-06-12 10:55:11 +0200

    chore: add todos for tests

---
+++ test/mediator_integration_test.dart
@@ -0,0 +1,6 @@
+// TODO: add integration tests for mediator
+// Alice and Bob wallets should be created based on Environement variables to have stable wallets
+// Check with Atlas team if they have some stable mediator on production and ask to share its DID Document URL
+// This DID Document URL should be read from environment variable
+
+void main() {}
