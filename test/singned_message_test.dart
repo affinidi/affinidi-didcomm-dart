4608593d68eef2daee5bdd70e54e7c9e907aa102
Kostiantyn Synyshyn <kostiantyn.s@affinidi.com>
2025-06-17 01:15:11 +0300

    feat: add basic test for signed message

--- test/singned_message_test.dart
+++ test/singned_message_test.dart
@@ -1,4 +1,75 @@
-// TODO: tests for SignedMessage. There is no difference for persistent and Bit32 wallets,
-// so P curves and Edward curves can be tested together
-// consider if we need to test both wallets on Persistent is good enough
-void main() {}
+import 'package:didcomm/didcomm.dart';
+import 'package:ssi/ssi.dart';
+import 'package:test/test.dart';
+
+import 'utils/create_message_assertion.dart';
+
+void main() async {
+  group('Encrypted message', () {
+    final aliceKeyStore = InMemoryKeyStore();
+    final aliceWallet = PersistentWallet(aliceKeyStore);
+
+    group('Persisted wallet', () {
+      for (final keyType in [
+        KeyType.p256,
+      ]) {
+        for (final signatureScheme in [
+          SignatureScheme.ecdsa_p256_sha256,
+        ]) {
+          group(keyType.name, () {
+            final aliceKeyId = 'alice-key-1-${keyType.name}';
+
+            late DidDocument aliceDidDocument;
+            late DidSigner aliceSigner;
+
+            setUp(() async {
+              final aliceKeyPair = await aliceWallet.generateKey(
+                keyId: aliceKeyId,
+                keyType: keyType,
+              );
+
+              aliceDidDocument =
+                  DidKey.generateDocument(aliceKeyPair.publicKey);
+
+              aliceSigner = DidSigner(
+                didDocument: aliceDidDocument,
+                keyPair: aliceKeyPair,
+                didKeyId: aliceDidDocument.verificationMethod[0].id,
+                signatureScheme: signatureScheme,
+              );
+            });
+
+            test('Pack and unpack encrypted message successfully', () async {
+              // Act: create and sign the message
+              const content = 'Hello, Bob!';
+              final plainTextMessage =
+                  await MessageAssertionService.createPlainTextMessageAssertion(
+                content,
+                from: aliceDidDocument.id,
+                to: ['did:rand:0x1234567890abcdef1234567890abcdef12345678'],
+              );
+
+              final signedMessage = await SignedMessage.pack(
+                plainTextMessage,
+                signer: aliceSigner,
+              );
+              print(signedMessage.toJson());
+
+              expect(signedMessage.signatures, isNotNull);
+
+              expect(
+                signedMessage.signatures[0].header.keyId,
+                aliceSigner.keyId,
+              );
+              final unpackedSignedMessage = await signedMessage.unpack();
+
+              expect(unpackedSignedMessage, isNotNull);
+              print(unpackedSignedMessage);
+              expect(unpackedSignedMessage['body']['content'], content);
+            });
+          });
+        }
+      }
+    });
+  });
+}
