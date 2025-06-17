321d9d27894cb0aca6359b822c3c433557775f63
Ivan Branets <ivan.b@affinidi.com>
2025-06-17 16:06:42 +0200

    chore: add partyU check

--- test/encrypted_message_with_persistent_wallet_test.dart
+++ test/encrypted_message_with_persistent_wallet_test.dart
@@ -120,10 +120,13 @@ void main() async {
                       );
 
                       // make sure sender identity does not leak for anonymous authentication
-                      expect(
-                        actualJweHeader.subjectKeyId,
-                        isAuthenticated ? isNotNull : isNull,
-                      );
+                      if (isAuthenticated) {
+                        expect(actualJweHeader.subjectKeyId, isNotNull);
+                        expect(actualJweHeader.agreementPartyUInfo, isNotNull);
+                      } else {
+                        expect(actualJweHeader.subjectKeyId, isNull);
+                        expect(actualJweHeader.agreementPartyUInfo, isNull);
+                      }
                     },
                   );
 
