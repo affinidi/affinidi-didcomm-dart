dc455d2ad75afc3e49d756c7bb16c2055a73075a
Sebastian Mueller <sebastian.m@affinidi.com>
2025-06-18 14:54:31 +0200

    feat: add attachments, jwks and algorithms to exports

--- lib/didcomm.dart
+++ lib/didcomm.dart
@@ -1,9 +1,12 @@
 library;
 
 export 'src/messages/didcomm_message.dart';
+export 'src/messages/algorithm_types/algorithms_types.dart';
+export 'src/messages/attachments.dart';
 export 'src/messages/core.dart';
 export 'src/messages/protocols.dart';
 export 'src/errors/errors.dart';
 export 'src/mediator_client.dart';
 export 'src/extensions/affinidi_authenticator_extension.dart';
 export 'src/extensions/wallet_extension.dart';
+export 'src/jwks/jwks.dart';
