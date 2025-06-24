fd6d6e5a3b9971151ceea7697c43792bceec5ad1
Kostiantyn Synyshyn <kostiantyn.s@affinidi.com>
2025-06-24 15:24:13 +0300

    feat: update after review

--- lib/src/messages/protocols/out_of_band/out_of_band_message.dart
+++ lib/src/messages/protocols/out_of_band/out_of_band_message.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'package:json_annotation/json_annotation.dart';
 
-import '../../../../didcomm.dart';
+import '../../attachments/attachment.dart';
+import '../../didcomm_message.dart';
 
 part 'out_of_band_message.g.dart';
 
@@ -10,18 +11,14 @@ class OutOfBandMessage extends DidcommMessage {
   final Uri type;
   final String id;
   final String from;
-  final String goal;
-  final String goalCode;
   final Map<String, dynamic>? body;
   final List<Attachment>? attachments;
 
   OutOfBandMessage({
     required this.id,
     required this.from,
-    required this.goal,
-    required this.goalCode,
-    required this.body,
     this.attachments,
+    this.body,
   }) : type = Uri.parse("https://didcomm.org/out-of-band/2.0/invitation");
 
   static const maxUrlCharactersLength = 2048;
@@ -30,7 +27,7 @@ class OutOfBandMessage extends DidcommMessage {
   /// Converts the OutOfBandMessage to a URL according to the DIDComm spec.
   /// [origin] must be a valid URL and non-empty, ie. https://example.com
   /// Throws [FormatException] if origin is invalid or URL exceeds 2048 chars.
-  String toURL({required String origin}) {
+  Uri toURL({required String origin}) {
     if (origin.trim().isEmpty) {
       throw ArgumentError('Origin must not be empty');
     }
@@ -38,17 +35,19 @@ class OutOfBandMessage extends DidcommMessage {
     if (uri == null || (!uri.hasScheme || !uri.hasAuthority)) {
       throw ArgumentError('Origin must be a valid absolute URL');
     }
-    final jsonStr = jsonEncode(toJson());
-    final compactJson = jsonStr.replaceAll(RegExp(r'\s+'), '');
-    final encoded = base64Url.encode(utf8.encode(compactJson));
+    final jsonEncodedMessage = jsonEncode(toJson());
+    final base64EncodedMessage =
+        base64Url.encode(utf8.encode(jsonEncodedMessage));
 
     final url = uri.replace(queryParameters: {
       ...uri.queryParameters,
-      oobQueryParam: encoded,
-    }).toString();
-    if (url.length > maxUrlCharactersLength) {
+      OutOfBandMessage.oobQueryParam: base64EncodedMessage,
+    });
+
+    final urlLength = url.toString().length;
+    if (urlLength > maxUrlCharactersLength) {
       throw FormatException(
-          'Resulting URL exceeds $maxUrlCharactersLength characters');
+          'Resulting URL exceeds $maxUrlCharactersLength characters limit. Current length: $urlLength');
     }
     return url;
   }
@@ -65,12 +64,12 @@ class OutOfBandMessage extends DidcommMessage {
     if (oobParam == null) {
       throw FormatException('URL does not contain the OOB query parameter');
     }
-    final decoded = utf8.decode(base64Url.decode(oobParam));
-    final json = jsonDecode(decoded);
-    if (json is! Map<String, dynamic>) {
+    final encodedMessage = utf8.decode(base64Url.decode(oobParam));
+    final jsonDecodedMessage = jsonDecode(encodedMessage);
+    if (jsonDecodedMessage is! Map<String, dynamic>) {
       throw FormatException('Decoded OOB parameter is not a valid JSON object');
     }
-    return OutOfBandMessage.fromJson(json);
+    return OutOfBandMessage.fromJson(jsonDecodedMessage);
   }
 
   factory OutOfBandMessage.fromJson(Map<String, dynamic> json) =>
--- lib/src/messages/protocols/out_of_band/out_of_band_message.g.dart
+++ lib/src/messages/protocols/out_of_band/out_of_band_message.g.dart
@@ -10,20 +10,16 @@ OutOfBandMessage _$OutOfBandMessageFromJson(Map<String, dynamic> json) =>
     OutOfBandMessage(
       id: json['id'] as String,
       from: json['from'] as String,
-      goal: json['goal'] as String,
-      goalCode: json['goalCode'] as String,
-      body: json['body'] as Map<String, dynamic>?,
       attachments: (json['attachments'] as List<dynamic>?)
           ?.map((e) => Attachment.fromJson(e as Map<String, dynamic>))
           .toList(),
+      body: json['body'] as Map<String, dynamic>?,
     );
 
 Map<String, dynamic> _$OutOfBandMessageToJson(OutOfBandMessage instance) =>
     <String, dynamic>{
       'id': instance.id,
       'from': instance.from,
-      'goal': instance.goal,
-      'goalCode': instance.goalCode,
       if (instance.body case final value?) 'body': value,
       if (instance.attachments?.map((e) => e.toJson()).toList()
           case final value?)
