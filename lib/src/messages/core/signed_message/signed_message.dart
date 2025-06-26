e485ff14fdaf1b6cc2df236f1b0212f8520137c6
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 16:27:23 +0200

    chore: add doc

--- lib/src/messages/core/signed_message/signed_message.dart
+++ lib/src/messages/core/signed_message/signed_message.dart
@@ -13,19 +13,36 @@ import '../../jwm.dart';
 part 'signed_message.g.dart';
 part 'signed_message.own_json_props.g.dart';
 
+/// Represents a DIDComm v2 Signed Message as defined in the DIDComm Messaging specification.
+///
+/// See: https://identity.foundation/didcomm-messaging/spec/#didcomm-signed-messages
 @OwnJsonProperties()
 @JsonSerializable(includeIfNull: false, explicitToJson: true)
 class SignedMessage extends DidcommMessage {
+  /// The default media type for signed DIDComm messages as per the spec.
   static final mediaType = 'application/didcomm-signed+json';
 
+  /// The base64url-encoded payload (the inner message).
   final String payload;
+
+  /// List of signatures over the payload.
   final List<Signature> signatures;
 
+  /// Constructs a [SignedMessage].
+  ///
+  /// [payload]: The base64url-encoded payload.
+  /// [signatures]: List of signatures over the payload.
   SignedMessage({
     required this.payload,
     required this.signatures,
   });
 
+  /// Packs a [DidcommMessage] into a [SignedMessage] using the provided [signer].
+  ///
+  /// [message]: The message to sign.
+  /// [signer]: The signer to use for signing the message.
+  ///
+  /// Returns a [SignedMessage] containing the signed payload.
   static Future<SignedMessage> pack(
     DidcommMessage message, {
     required DidSigner signer,
@@ -58,6 +75,11 @@ class SignedMessage extends DidcommMessage {
     return signedMessage;
   }
 
+  /// Unpacks the signed message and verifies signature, returning the inner message as a JSON map.
+  /// Unlike [DidcommMessage.unpackToPlainTextMessage], this method does not recursively unpack nested messages, but
+  /// returns the top most message from the payload.
+  ///
+  /// Throws an [Exception] if the signature is invalid.
   Future<Map<String, dynamic>> unpack() async {
     if (!(await areSignaturesValid())) {
       Exception('Invalid signature was found');
@@ -69,7 +91,9 @@ class SignedMessage extends DidcommMessage {
     return innerMessage as Map<String, dynamic>;
   }
 
-  // TODO: add issuer check
+  /// Verifies all signatures in the message.
+  ///
+  /// Returns true if all signatures are valid, false otherwise.
   Future<bool> areSignaturesValid() async {
     for (final signature in signatures) {
       final signatureScheme =
@@ -101,10 +125,14 @@ class SignedMessage extends DidcommMessage {
     return true;
   }
 
+  /// Checks if the given [message] map is a signed message by verifying required properties.
   static bool isSignedMessage(Map<String, dynamic> message) {
     return _$ownJsonProperties.every((prop) => message.containsKey(prop));
   }
 
+  /// Creates a [SignedMessage] from a JSON map.
+  ///
+  /// [json]: The JSON map representing the signed message.
   factory SignedMessage.fromJson(Map<String, dynamic> json) {
     final message = _$SignedMessageFromJson(json)
       ..assignCustomHeaders(json, _$ownJsonProperties);
@@ -112,6 +140,7 @@ class SignedMessage extends DidcommMessage {
     return message;
   }
 
+  /// Serializes the signed message to a JSON map, including custom headers.
   @override
   Map<String, dynamic> toJson() =>
       withCustomHeaders(_$SignedMessageToJson(this));
