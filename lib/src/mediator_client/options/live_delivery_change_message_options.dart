68c4f46f3660d5ae170c77c34647d6122509b17a
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 13:47:17 +0200

    chore: doc

--- lib/src/mediator_client/options/live_delivery_change_message_options.dart
+++ lib/src/mediator_client/options/live_delivery_change_message_options.dart
@@ -1,8 +1,21 @@
 import 'message_options.dart';
 
+/// Options for sending a live delivery change message over a DIDComm mediator.
+///
+/// Allows configuration of whether to send the message and how it should be protected (signing, encryption, etc).
 class LiveDeliveryChangeMessageOptions extends MessageOptions {
+  /// Whether a live delivery change message should be sent.
+  ///
+  /// Default is false.
   final bool shouldSend;
 
+  /// Constructs [LiveDeliveryChangeMessageOptions].
+  ///
+  /// [shouldSend]: Whether to send the live delivery change message (default: false).
+  /// [shouldSign]: Whether the message should be signed (inherited from [MessageOptions]).
+  /// [shouldEncrypt]: Whether the message should be encrypted (inherited from [MessageOptions]).
+  /// [encryptionAlgorithm]: The encryption algorithm to use (inherited from [MessageOptions]).
+  /// [keyWrappingAlgorithm]: The key wrapping algorithm to use (inherited from [MessageOptions]).
   const LiveDeliveryChangeMessageOptions({
     this.shouldSend = false,
     super.shouldSign,
--- lib/src/mediator_client/options/status_request_message_options.dart
+++ lib/src/mediator_client/options/status_request_message_options.dart
@@ -1,8 +1,21 @@
 import 'message_options.dart';
 
+/// Options for sending a status request message over a DIDComm mediator.
+///
+/// Allows configuration of whether to send the message and how it should be protected (signing, encryption, etc).
 class StatusRequestMessageOptions extends MessageOptions {
+  /// Whether a status request message should be sent.
+  ///
+  /// Default is false.
   final bool shouldSend;
 
+  /// Constructs [StatusRequestMessageOptions].
+  ///
+  /// [shouldSend]: Whether to send the status request message (default: false).
+  /// [shouldSign]: Whether the message should be signed (inherited from [MessageOptions]).
+  /// [shouldEncrypt]: Whether the message should be encrypted (inherited from [MessageOptions]).
+  /// [encryptionAlgorithm]: The encryption algorithm to use (inherited from [MessageOptions]).
+  /// [keyWrappingAlgorithm]: The key wrapping algorithm to use (inherited from [MessageOptions]).
   const StatusRequestMessageOptions({
     this.shouldSend = false,
     super.shouldSign,
