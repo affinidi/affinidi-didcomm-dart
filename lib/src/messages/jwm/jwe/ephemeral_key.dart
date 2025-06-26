942b0d8cf2d28fe5c845899119d684fb8842254f
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 11:35:14 +0200

    chore: ephemeral key docs

--- lib/src/messages/jwm/jwe/ephemeral_key.dart
+++ lib/src/messages/jwm/jwe/ephemeral_key.dart
@@ -8,20 +8,26 @@ import 'ephemeral_key_type.dart';
 
 part 'ephemeral_key.g.dart';
 
-// TODO: replace with JWK from this package
+/// Represents an ephemeral public key used in JWE (JSON Web Encryption) messages.
 @JsonSerializable(includeIfNull: false, explicitToJson: true)
 class EphemeralKey {
+  /// The key type (e.g., OKP, EC).
   @JsonKey(name: 'kty')
   final EphemeralKeyType keyType;
+
+  /// The cryptographic curve used (e.g., Ed25519, P-256).
   @JsonKey(name: 'crv')
   final CurveType curve;
 
+  /// The public key's x coordinate, base64url encoded.
   @Base64UrlConverter()
   final Uint8List x;
 
+  /// The public key's y coordinate, base64url encoded (optional).
   @Base64UrlConverter()
   final Uint8List? y;
 
+  /// Creates an [EphemeralKey] instance.
   EphemeralKey({
     required this.keyType,
     required this.curve,
@@ -29,8 +35,10 @@ class EphemeralKey {
     this.y,
   });
 
+  /// Creates an [EphemeralKey] from a JSON map.
   factory EphemeralKey.fromJson(Map<String, dynamic> json) =>
       _$EphemeralKeyFromJson(json);
 
+  /// Converts this [EphemeralKey] to a JSON map.
   Map<String, dynamic> toJson() => _$EphemeralKeyToJson(this);
 }
