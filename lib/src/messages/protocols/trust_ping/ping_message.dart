9edc80b35dcdccbe427c00366e714013fc16267d
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 17:12:42 +0200

    chore: add trusted ping

--- lib/src/messages/protocols/trust_ping/ping_message.dart
+++ lib/src/messages/protocols/trust_ping/ping_message.dart
@@ -1,15 +1,41 @@
 import 'package:json_annotation/json_annotation.dart';
-import '../../didcomm_message.dart';
+import '../../core/plain_text_message/plain_text_message.dart';
 
-part 'ping_message.g.dart';
-
+/// Represents a DIDComm v2 Trust Ping message as defined in the trust-ping protocol.
+///
+/// See: https://identity.foundation/didcomm-messaging/spec/#trust-ping-protocol-20
 @JsonSerializable(includeIfNull: false, explicitToJson: true)
-class PingMessage extends DidcommMessage {
-  PingMessage();
+class PingMessage extends PlainTextMessage {
+  /// Indicates whether a response is requested from the recipient.
+  final bool responseRequested;
 
-  factory PingMessage.fromJson(Map<String, dynamic> json) =>
-      _$PingMessageFromJson(json);
+  /// Constructs a [PingMessage].
+  ///
+  /// [id]: Unique message identifier.
+  /// [from]: Sender's DID.
+  /// [to]: List of recipient DIDs (optional).
+  /// [responseRequested]: Whether a response is requested (default: true).
+  PingMessage({
+    required super.id,
+    required super.from,
+    super.to,
+    this.responseRequested = true,
+  }) : super(
+          type: Uri.parse('https://didcomm.org/trust-ping/2.0/ping'),
+          body: {'response_requested': responseRequested},
+        );
 
-  @override
-  Map<String, dynamic> toJson() => _$PingMessageToJson(this);
+  /// Creates a [PingMessage] from a JSON map.
+  ///
+  /// [json]: The JSON map representing the ping message.
+  factory PingMessage.fromJson(Map<String, dynamic> json) {
+    final plainTextMessage = PlainTextMessage.fromJson(json);
+    return PingMessage(
+      id: plainTextMessage.id,
+      to: plainTextMessage.to,
+      from: plainTextMessage.from,
+      responseRequested:
+          plainTextMessage.body?['response_requested'] as bool? ?? false,
+    );
+  }
 }
