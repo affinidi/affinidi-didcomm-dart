019ffde477abfe9503f2b7ca0e19bb87f333b541
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 15:31:48 +0200

    chore: add doc

--- lib/src/converters/jws_header_converter.dart
+++ lib/src/converters/jws_header_converter.dart
@@ -5,9 +5,17 @@ import '../common/encoding.dart';
 import '../extensions/extensions.dart';
 import '../messages/jwm.dart';
 
+/// A [JsonConverter] for encoding and decoding [JwsHeader] objects as base64url strings.
+///
+/// This converter is used to serialize and deserialize JWS protected headers in DIDComm messages.
 class JwsHeaderConverter implements JsonConverter<JwsHeader, String> {
+  /// Creates a [JwsHeaderConverter].
   const JwsHeaderConverter();
 
+  /// Decodes a [JwsHeader] from a base64url-encoded JSON string.
+  ///
+  /// [base64UrlInput]: The base64url-encoded string representing the JWS header.
+  /// Returns the decoded [JwsHeader] object.
   @override
   JwsHeader fromJson(String base64UrlInput) {
     final bytes = base64UrlDecodeWithPadding(base64UrlInput);
@@ -18,6 +26,10 @@ class JwsHeaderConverter implements JsonConverter<JwsHeader, String> {
     );
   }
 
+  /// Encodes a [JwsHeader] as a base64url-encoded JSON string.
+  ///
+  /// [object]: The [JwsHeader] to encode.
+  /// Returns the base64url-encoded string.
   @override
   String toJson(JwsHeader object) =>
       base64UrlEncodeNoPadding(object.toJsonBytes());
