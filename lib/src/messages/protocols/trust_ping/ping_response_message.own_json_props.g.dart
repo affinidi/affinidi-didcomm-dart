7eaf808c9360b7a6b478773be5141b8207258e94
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 17:33:34 +0200

    chore: add doc

---
+++ lib/src/messages/protocols/trust_ping/ping_response_message.own_json_props.g.dart
@@ -0,0 +1,21 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ping_response_message.dart';
+
+// **************************************************************************
+// OwnJsonPropertiesGenerator
+// **************************************************************************
+
+const _$ownJsonProperties = [
+  'id',
+  'type',
+  'from',
+  'to',
+  'thid',
+  'pthid',
+  'created_time',
+  'expires_time',
+  'body',
+  'attachments'
+];
