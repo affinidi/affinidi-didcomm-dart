be74d331dabdd41cb935affcb4ac4123aa464311
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 13:23:19 +0200

    chore: add doc

--- lib/src/messages/algorithm_types/key_wrapping_algorithm.dart
+++ lib/src/messages/algorithm_types/key_wrapping_algorithm.dart
@@ -1,10 +1,20 @@
 import 'package:json_annotation/json_annotation.dart';
 
+/// Supported key wrapping algorithms for DIDComm encryption as defined in
+/// [DIDComm Messaging Spec, Key Wrapping Algorithms](https://identity.foundation/didcomm-messaging/spec/#key-wrapping-algorithms).
 @JsonEnum(valueField: 'value')
 enum KeyWrappingAlgorithm {
+  /// ECDH-ES with AES Key Wrap (A256KW).
   ecdhEs('ECDH-ES+A256KW'),
+
+  /// ECDH-1PU with AES Key Wrap (A256KW).
   ecdh1Pu('ECDH-1PU+A256KW');
 
+  /// The string value of the key wrapping algorithm, as used in JWE headers.
   final String value;
+
+  /// Constructs a [KeyWrappingAlgorithm] enum value.
+  ///
+  /// [value]: The string representation of the algorithm.
   const KeyWrappingAlgorithm(this.value);
 }
