4e1215ccd83cc1a877fd4d29f27a3ead67c75641
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 16:13:08 +0200

    chore: add doc

--- lib/builder.dart
+++ lib/builder.dart
@@ -2,5 +2,9 @@ import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 import 'src/generators/own_json_properties_generator.dart';
 
+/// Creates a [PartBuilder] for generating custom JSON property lists using [OwnJsonPropertiesGenerator].
+///
+/// [options]: The builder options provided by the build system.
+/// Returns a [PartBuilder] configured to generate '.own_json_props.g.dart' files.
 Builder ownJsonPropertiesBuilder(BuilderOptions options) =>
     PartBuilder([OwnJsonPropertiesGenerator()], '.own_json_props.g.dart');
