d5eeaedb50116663ae8f98a8db347099adadf32d
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 15:35:53 +0200

    chore: add doc

--- lib/src/common/crypto.dart
+++ lib/src/common/crypto.dart
@@ -8,6 +8,11 @@ import 'package:x25519/x25519.dart' as x25519;
 import '../errors/errors.dart';
 import '../messages/algorithm_types/algorithms_types.dart';
 
+/// Generates an ephemeral key pair for the given [keyType].
+///
+/// [keyType]: The type of key to generate (e.g., p256, secp256k1, ed25519).
+/// Returns a record containing the private key bytes and, for ed25519, the public key bytes.
+/// Throws [UnsupportedKeyTypeError] if the key type is not supported.
 ({Uint8List privateKeyBytes, Uint8List? publicKeyBytes})
     generateEphemeralKeyPair(KeyType keyType) {
   if (keyType == KeyType.p256) {
@@ -39,6 +44,11 @@ import '../messages/algorithm_types/algorithms_types.dart';
   throw UnsupportedKeyTypeError(keyType);
 }
 
+/// Returns an [ec.PrivateKey] from the given [bytes] for the specified [keyType].
+///
+/// [bytes]: The private key bytes.
+/// [keyType]: The type of key (must be p256 or secp256k1).
+/// Throws [UnsupportedKeyTypeError] if the key type is not supported.
 ec.PrivateKey getPrivateKeyFromBytes(
   Uint8List bytes, {
   required KeyType keyType,
@@ -54,6 +64,11 @@ ec.PrivateKey getPrivateKeyFromBytes(
   throw UnsupportedKeyTypeError(keyType);
 }
 
+/// Creates a symmetric [ck.Encrypter] for the given [encryptionAlgorithm] and [encryptionKey].
+///
+/// [encryptionAlgorithm]: The encryption algorithm to use (A256CBC-HS512 or A256GCM).
+/// [encryptionKey]: The symmetric key to use for encryption.
+/// Throws [UnsupportedEncryptionAlgorithmError] if the algorithm is not supported.
 ck.Encrypter createSymmetricEncrypter(
   EncryptionAlgorithm encryptionAlgorithm,
   ck.SymmetricKey encryptionKey,
--- lib/src/curves/curve_type.dart
+++ lib/src/curves/curve_type.dart
@@ -1,18 +1,31 @@
 import 'package:json_annotation/json_annotation.dart';
 
+/// Supported elliptic curve types for DIDComm cryptography.
 @JsonEnum(valueField: 'value')
 enum CurveType {
+  /// NIST P-256 curve (also known as secp256r1).
   p256('P-256'),
+
+  /// SECG secp256k1 curve.
   secp256k1('secp256k1'),
+
+  /// X25519 curve.
   x25519('X25519');
 
+  /// The string value of the curve, as used in JWKs and headers.
   final String value;
+
+  /// Constructs a [CurveType] enum value.
+  ///
+  /// [value]: The string representation of the curve.
   const CurveType(this.value);
 
+  /// Returns true if this curve is a P-curve or secp256k1 (used for ECDH-ES/ECDH-1PU).
   bool isSecp256OrPCurve() {
     return value.startsWith('P') || value.startsWith('secp256k');
   }
 
+  /// Returns true if this curve is an X-curve (X25519).
   bool isXCurve() {
     return value.startsWith('X');
   }
