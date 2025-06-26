7f090266cd919030e2a877ea6370a2c145403ab2
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 13:40:34 +0200

    chore: add doc

--- lib/src/messages/protocols/message_pickup/live_delivery_change_message.dart
+++ lib/src/messages/protocols/message_pickup/live_delivery_change_message.dart
@@ -1,8 +1,24 @@
 import 'message_pickup_message/message_pickup_message.dart';
 
+/// Represents a DIDComm Message Pickup 3.0 Live Delivery Change message as defined in
+/// [DIDComm Messaging Spec, Message Pickup Protocol 3.0](https://didcomm.org/messagepickup/3.0).
+///
+/// This message is used to request or change the live delivery mode for message pickup.
 class LiveDeliveryChangeMessage extends MessagePickupMessage {
+  /// Indicates whether live delivery is requested (true) or not (false).
+  ///
+  /// See [DIDComm Message Pickup Protocol 3.0](https://didcomm.org/messagepickup/3.0)
+  /// for the semantics of the `live_delivery` field.
   final bool liveDelivery;
 
+  /// Constructs a [LiveDeliveryChangeMessage].
+  ///
+  /// [id]: Unique identifier for the message.
+  /// [to]: List of recipient DIDs.
+  /// [liveDelivery]: Whether live delivery is requested.
+  /// [from]: Sender's DID.
+  /// [expiresTime]: Optional expiration time for the message.
+  /// [returnRoute]: Optional return route header value.
   LiveDeliveryChangeMessage({
     required super.id,
     required super.to,
@@ -17,6 +33,9 @@ class LiveDeliveryChangeMessage extends MessagePickupMessage {
           body: {'live_delivery': liveDelivery},
         );
 
+  /// Creates a [LiveDeliveryChangeMessage] from a JSON map.
+  ///
+  /// [json]: The JSON map representing the message.
   factory LiveDeliveryChangeMessage.fromJson(Map<String, dynamic> json) {
     final plainTextMessage = MessagePickupMessage.fromJson(json);
     return LiveDeliveryChangeMessage(
--- lib/src/messages/protocols/message_pickup/status_request_message.dart
+++ lib/src/messages/protocols/message_pickup/status_request_message.dart
@@ -1,9 +1,25 @@
 import '../../core.dart';
 import 'message_pickup_message/message_pickup_message.dart';
 
+/// Represents a DIDComm Message Pickup 3.0 Status Request message as defined in
+/// [DIDComm Messaging Spec, Message Pickup Protocol 3.0](https://didcomm.org/messagepickup/3.0/).
+///
+/// This message is used to request the status of pending messages for a specific recipient DID.
 class StatusRequestMessage extends MessagePickupMessage {
+  /// The DID of the recipient whose message status is being requested.
+  ///
+  /// See [DIDComm Message Pickup Protocol 3.0](https://didcomm.org/messagepickup/3.0/)
+  /// for the semantics of the `recipient_did` field.
   final String recipientDid;
 
+  /// Constructs a [StatusRequestMessage].
+  ///
+  /// [id]: Unique identifier for the message.
+  /// [to]: List of recipient DIDs.
+  /// [recipientDid]: The DID whose message status is being requested.
+  /// [from]: Sender's DID.
+  /// [expiresTime]: Optional expiration time for the message.
+  /// [returnRoute]: Optional return route header value.
   StatusRequestMessage({
     required super.id,
     required super.to,
@@ -18,6 +34,9 @@ class StatusRequestMessage extends MessagePickupMessage {
           body: {'recipient_did': recipientDid},
         );
 
+  /// Creates a [StatusRequestMessage] from a JSON map.
+  ///
+  /// [json]: The JSON map representing the message.
   factory StatusRequestMessage.fromJson(Map<String, dynamic> json) {
     final plainTextMessage = PlainTextMessage.fromJson(json);
     return StatusRequestMessage(
