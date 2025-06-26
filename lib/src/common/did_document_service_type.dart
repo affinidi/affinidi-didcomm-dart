be73fcc023ae7e78183d91699b147636e70697b8
Ivan Branets <ivan.b@affinidi.com>
2025-06-26 13:08:23 +0200

    chore: add doc

--- lib/src/common/did_document_service_type.dart
+++ lib/src/common/did_document_service_type.dart
@@ -1,7 +1,18 @@
+/// Enum representing supported service types in a DID Document.
+///
+/// - [didCommMessaging]: Service endpoint for DIDComm Messaging.
+/// - [authentication]: Service endpoint for authentication purposes.
+// TODO: prevent failure for unlisted service types
 enum DidDocumentServiceType {
+  /// Service endpoint for DIDComm Messaging ("DIDCommMessaging").
   didCommMessaging('DIDCommMessaging'),
+
+  /// Service endpoint for authentication ("Authentication").
   authentication('Authentication');
 
+  /// The string value as used in the DID Document service type.
   final String value;
+
+  /// Constructs a [DidDocumentServiceType] with the given string [value].
   const DidDocumentServiceType(this.value);
 }
