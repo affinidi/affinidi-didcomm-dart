be6855dcc4c84150649373f1a485b3d22bd42326
Ivan Branets <ivan.b@affinidi.com>
2025-07-10 16:10:27 +0200

    chore: add more tests

--- test/singned_message_test.dart
+++ test/singned_message_test.dart
@@ -10,29 +10,6 @@ import 'package:test/test.dart';
 import 'utils/create_message_assertion.dart';
 
 void main() async {
-  final persistentWallet = PersistentWallet(
-    InMemoryKeyStore(),
-  );
-
-  final seed = List<int>.generate(
-    32,
-    (index) => index + 1,
-  );
-
-  final bip32Wallet = Bip32Wallet.fromSeed(Uint8List.fromList(seed));
-
-  final wallets = {
-    KeyType.p256: persistentWallet,
-    KeyType.ed25519: persistentWallet,
-    KeyType.secp256k1: bip32Wallet,
-  };
-
-  final keyIds = {
-    KeyType.p256: 'key-1-${KeyType.p256.name}',
-    KeyType.ed25519: 'key-1-${KeyType.ed25519.name}',
-    KeyType.secp256k1: "m/44'/60'/0'/0'/0'",
-  };
-
   group('Signed message', () {
     for (final keyType in [
       KeyType.p256,
@@ -40,215 +17,235 @@ void main() async {
       KeyType.secp256k1,
     ]) {
       group(keyType.name, () {
-        final keyId = keyIds[keyType]!;
+        for (final didType in [
+          'did:key',
+          'did:peer',
+        ]) {
+          group(didType, () {
+            final useDidKey = didType == 'did:key';
+            final keyId = 'key-1-${keyType.name}';
 
-        late DidManager didManager;
-        late DidDocument didDocument;
-        late DidSigner signer;
+            late DidManager didManager;
+            late DidDocument didDocument;
+            late DidSigner signer;
 
-        setUp(() async {
-          final wallet = wallets[keyType]!;
+            setUp(() async {
+              final wallet = PersistentWallet(
+                InMemoryKeyStore(),
+              );
 
-          didManager = DidKeyManager(
-            wallet: wallet,
-            store: InMemoryDidStore(),
-          );
+              didManager = useDidKey
+                  ? DidKeyManager(
+                      wallet: wallet,
+                      store: InMemoryDidStore(),
+                    )
+                  : DidPeerManager(
+                      wallet: wallet,
+                      store: InMemoryDidStore(),
+                    );
 
-          await wallet.generateKey(
-            keyId: keyId,
-            keyType: keyType,
-          );
+              await wallet.generateKey(
+                keyId: keyId,
+                keyType: keyType,
+              );
 
-          await didManager.addVerificationMethod(keyId);
-          didDocument = await didManager.getDidDocument();
+              await didManager.addVerificationMethod(keyId);
+              didDocument = await didManager.getDidDocument();
 
-          signer = await didManager.getSigner(
-            didDocument.assertionMethod.first.id,
-          );
-        });
+              signer = await didManager.getSigner(
+                didDocument.assertionMethod.first.id,
+              );
+            });
 
-        test('Pack and unpack signed message successfully', () async {
-          const content = 'Hello, Bob!';
-          final plainTextMessage =
-              MessageAssertionService.createPlainTextMessageAssertion(
-            content,
-            from: didDocument.id,
-            to: ['did:rand:0x1234567890abcdef1234567890abcdef12345678'],
-          );
+            test('Pack and unpack signed message successfully', () async {
+              const content = 'Hello, Bob!';
+              final plainTextMessage =
+                  MessageAssertionService.createPlainTextMessageAssertion(
+                content,
+                from: didDocument.id,
+                to: ['did:rand:0x1234567890abcdef1234567890abcdef12345678'],
+              );
 
-          final signedMessage = await SignedMessage.pack(
-            plainTextMessage,
-            signer: signer,
-          );
+              final signedMessage = await SignedMessage.pack(
+                plainTextMessage,
+                signer: signer,
+              );
 
-          expect(signedMessage.signatures, isNotNull);
-          expect(
-            signedMessage.signatures.first.header.keyId,
-            didDocument.assertionMethod.first.didKeyId,
-          );
+              expect(signedMessage.signatures, isNotNull);
+              expect(
+                signedMessage.signatures.first.header.keyId,
+                didDocument.assertionMethod.first.didKeyId,
+              );
 
-          final unpackedPlainTextMessage =
-              await DidcommMessage.unpackToPlainTextMessage(
-            message: signedMessage.toJson(),
-            recipientDidManager: didManager,
-            validateAddressingConsistency: true,
-            expectedMessageWrappingTypes: [
-              MessageWrappingType.signedPlaintext,
-            ],
-            expectedSigners: [
-              didDocument.assertionMethod.first.didKeyId,
-            ],
-          );
+              final unpackedPlainTextMessage =
+                  await DidcommMessage.unpackToPlainTextMessage(
+                message: signedMessage.toJson(),
+                recipientDidManager: didManager,
+                validateAddressingConsistency: true,
+                expectedMessageWrappingTypes: [
+                  MessageWrappingType.signedPlaintext,
+                ],
+                expectedSigners: [
+                  didDocument.assertionMethod.first.didKeyId,
+                ],
+              );
 
-          expect(unpackedPlainTextMessage, isNotNull);
-          expect(unpackedPlainTextMessage.body!['content'], content);
-        });
+              expect(unpackedPlainTextMessage, isNotNull);
+              expect(unpackedPlainTextMessage.body!['content'], content);
+            });
 
-        test('Should fail on invalid signature if there is only one signature',
-            () async {
-          // Act: create and sign the message
-          const content = 'Hello, Bob!';
-          final plainTextMessage =
-              MessageAssertionService.createPlainTextMessageAssertion(
-            content,
-            from: didDocument.id,
-            to: ['did:rand:0x1234567890abcdef1234567890abcdef12345678'],
-          );
+            test(
+                'Should fail on invalid signature if there is only one signature',
+                () async {
+              // Act: create and sign the message
+              const content = 'Hello, Bob!';
+              final plainTextMessage =
+                  MessageAssertionService.createPlainTextMessageAssertion(
+                content,
+                from: didDocument.id,
+                to: ['did:rand:0x1234567890abcdef1234567890abcdef12345678'],
+              );
 
-          final signedMessage = await SignedMessage.pack(
-            plainTextMessage,
-            signer: signer,
-          );
+              final signedMessage = await SignedMessage.pack(
+                plainTextMessage,
+                signer: signer,
+              );
 
-          final brokenMessage = signedMessage.toJson();
+              final brokenMessage = signedMessage.toJson();
 
-          // simulate invalid signature
-          brokenMessage['payload'] = base64Encode(
-            {'content': 'invalid data'}.toJsonBytes(),
-          );
+              // simulate invalid signature
+              brokenMessage['payload'] = base64Encode(
+                {'content': 'invalid data'}.toJsonBytes(),
+              );
 
-          final actualFuture = DidcommMessage.unpackToPlainTextMessage(
-            message: brokenMessage,
-            recipientDidManager: didManager,
-            validateAddressingConsistency: true,
-            expectedMessageWrappingTypes: [
-              MessageWrappingType.signedPlaintext,
-            ],
-            expectedSigners: [
-              didDocument.assertionMethod.first.didKeyId,
-            ],
-          );
+              final actualFuture = DidcommMessage.unpackToPlainTextMessage(
+                message: brokenMessage,
+                recipientDidManager: didManager,
+                validateAddressingConsistency: true,
+                expectedMessageWrappingTypes: [
+                  MessageWrappingType.signedPlaintext,
+                ],
+                expectedSigners: [
+                  didDocument.assertionMethod.first.didKeyId,
+                ],
+              );
 
-          await expectLater(actualFuture, throwsA(isA<Exception>()));
-        });
+              await expectLater(actualFuture, throwsA(isA<Exception>()));
+            });
 
-        test(
-            'Pack and unpack encrypted message with multiple signatures successfully',
-            () async {
-          final extraDidManager = DidKeyManager(
-            store: InMemoryDidStore(),
-            wallet: PersistentWallet(InMemoryKeyStore()),
-          );
+            test(
+                'Pack and unpack encrypted message with multiple signatures successfully',
+                () async {
+              final extraDidManager = DidKeyManager(
+                store: InMemoryDidStore(),
+                wallet: PersistentWallet(InMemoryKeyStore()),
+              );
 
-          final extraKeyPair = await extraDidManager.wallet.generateKey(
-            // extra wallet always has p256
-            keyType: KeyType.p256,
-          );
+              final extraKeyPair = await extraDidManager.wallet.generateKey(
+                // extra wallet always has p256
+                keyType: KeyType.p256,
+              );
 
-          await extraDidManager.addVerificationMethod(extraKeyPair.id);
+              await extraDidManager.addVerificationMethod(extraKeyPair.id);
 
-          final extraSigner = await extraDidManager.getSigner(
-            (await extraDidManager.getDidDocument()).assertionMethod.first.id,
-          );
+              final extraSigner = await extraDidManager.getSigner(
+                (await extraDidManager.getDidDocument())
+                    .assertionMethod
+                    .first
+                    .id,
+              );
 
-          const content = 'Hello, Bob!';
-          final plainTextMessage =
-              MessageAssertionService.createPlainTextMessageAssertion(
-            content,
-            from: didDocument.id,
-            to: ['did:rand:0x1234567890abcdef1234567890abcdef12345678'],
-          );
+              const content = 'Hello, Bob!';
+              final plainTextMessage =
+                  MessageAssertionService.createPlainTextMessageAssertion(
+                content,
+                from: didDocument.id,
+                to: ['did:rand:0x1234567890abcdef1234567890abcdef12345678'],
+              );
 
-          final signedMessage = await SignedMessage.pack(
-            plainTextMessage,
-            signer: signer,
-          );
+              final signedMessage = await SignedMessage.pack(
+                plainTextMessage,
+                signer: signer,
+              );
 
-          final extraSignedMessage = await SignedMessage.pack(
-            plainTextMessage,
-            signer: extraSigner,
-          );
+              final extraSignedMessage = await SignedMessage.pack(
+                plainTextMessage,
+                signer: extraSigner,
+              );
 
-          signedMessage.signatures.add(
-            extraSignedMessage.signatures.first,
-          );
+              signedMessage.signatures.add(
+                extraSignedMessage.signatures.first,
+              );
 
-          expect(signedMessage.signatures, isNotNull);
-          expect(
-            signedMessage.signatures.first.header.keyId,
-            didDocument.assertionMethod.first.didKeyId,
-          );
+              expect(signedMessage.signatures, isNotNull);
+              expect(
+                signedMessage.signatures.first.header.keyId,
+                didDocument.assertionMethod.first.didKeyId,
+              );
 
-          final unpackedPlainTextMessage =
-              await DidcommMessage.unpackToPlainTextMessage(
-            message: signedMessage.toJson(),
-            recipientDidManager: didManager,
-            validateAddressingConsistency: true,
-            expectedMessageWrappingTypes: [
-              MessageWrappingType.signedPlaintext,
-            ],
-            expectedSigners: [
-              signer.didKeyId,
-              extraSigner.didKeyId,
-            ],
-          );
+              final unpackedPlainTextMessage =
+                  await DidcommMessage.unpackToPlainTextMessage(
+                message: signedMessage.toJson(),
+                recipientDidManager: didManager,
+                validateAddressingConsistency: true,
+                expectedMessageWrappingTypes: [
+                  MessageWrappingType.signedPlaintext,
+                ],
+                expectedSigners: [
+                  signer.didKeyId,
+                  extraSigner.didKeyId,
+                ],
+              );
 
-          expect(unpackedPlainTextMessage, isNotNull);
-          expect(unpackedPlainTextMessage.body!['content'], content);
-        });
+              expect(unpackedPlainTextMessage, isNotNull);
+              expect(unpackedPlainTextMessage.body!['content'], content);
+            });
 
-        test('Should fail on invalid signature if there are multiple signature',
-            () async {
-          const content = 'Hello, Bob!';
-          final plainTextMessage =
-              MessageAssertionService.createPlainTextMessageAssertion(
-            content,
-            from: didDocument.id,
-            to: ['did:rand:0x1234567890abcdef1234567890abcdef12345678'],
-          );
+            test(
+                'Should fail on invalid signature if there are multiple signature',
+                () async {
+              const content = 'Hello, Bob!';
+              final plainTextMessage =
+                  MessageAssertionService.createPlainTextMessageAssertion(
+                content,
+                from: didDocument.id,
+                to: ['did:rand:0x1234567890abcdef1234567890abcdef12345678'],
+              );
 
-          final signedMessage = await SignedMessage.pack(
-            plainTextMessage,
-            signer: signer,
-          );
+              final signedMessage = await SignedMessage.pack(
+                plainTextMessage,
+                signer: signer,
+              );
 
-          final originalSignature = signedMessage.signatures.first;
+              final originalSignature = signedMessage.signatures.first;
 
-          // simulate invalid signature additionally to a valid signature
-          final fakeSignature = Signature(
-            protected: originalSignature.protected,
-            signature: Uint8List.fromList(
-              List.filled(originalSignature.signature.length, 5),
-            ),
-            header: originalSignature.header,
-          );
+              // simulate invalid signature additionally to a valid signature
+              final fakeSignature = Signature(
+                protected: originalSignature.protected,
+                signature: Uint8List.fromList(
+                  List.filled(originalSignature.signature.length, 5),
+                ),
+                header: originalSignature.header,
+              );
 
-          signedMessage.signatures.add(fakeSignature);
+              signedMessage.signatures.add(fakeSignature);
 
-          final actualFuture = DidcommMessage.unpackToPlainTextMessage(
-            message: signedMessage.toJson(),
-            recipientDidManager: didManager,
-            validateAddressingConsistency: true,
-            expectedMessageWrappingTypes: [
-              MessageWrappingType.signedPlaintext,
-            ],
-            expectedSigners: [
-              didDocument.assertionMethod.first.didKeyId,
-            ],
-          );
+              final actualFuture = DidcommMessage.unpackToPlainTextMessage(
+                message: signedMessage.toJson(),
+                recipientDidManager: didManager,
+                validateAddressingConsistency: true,
+                expectedMessageWrappingTypes: [
+                  MessageWrappingType.signedPlaintext,
+                ],
+                expectedSigners: [
+                  didDocument.assertionMethod.first.didKeyId,
+                ],
+              );
 
-          await expectLater(actualFuture, throwsA(isA<Exception>()));
-        });
+              await expectLater(actualFuture, throwsA(isA<Exception>()));
+            });
+          });
+        }
       });
     }
   });
