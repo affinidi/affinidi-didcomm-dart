9b54384310f94f5eb885774d15e6a72fc908768b
Ivan Branets <ivan.b@affinidi.com>
2025-07-18 00:33:42 +0200

    feat: add disclose message

--- lib/src/messages/protocols/discover_features/query.dart
+++ lib/src/messages/protocols/discover_features/query.dart
@@ -1,27 +1,10 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import 'feature_type.dart';
+
 part 'query.g.dart';
 
-@JsonEnum()
-
-/// Enum representing the type of feature in a DIDComm Discover Features query.
-@JsonEnum()
-enum FeatureType {
-  /// A protocol feature type.
-  protocol,
-
-  /// A goal-code feature type.
-  @JsonValue('goal-code')
-  goalCode,
-
-  /// A header feature type.
-  header,
-
-  /// Used when an unknown or unrecognized feature type is encountered during deserialization.
-  unknown,
-}
-
-/// Model for a DIDComm Discover Features query message.
+/// Model for a DIDComm discover features query.
 @JsonSerializable(includeIfNull: false, explicitToJson: true)
 class Query {
   /// The type of feature being queried.
