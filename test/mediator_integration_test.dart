78405c725d8e9a66d9e25feb09f7607c44a3eac8
Robert Kwolek <robert.k@affinidi.com>
2025-07-23 11:23:13 +0200

    feat: test evns vars updated

--- test/mediator_integration_test.dart
+++ test/mediator_integration_test.dart
@@ -7,47 +7,14 @@ import 'package:ssi/ssi.dart';
 import 'package:test/test.dart';
 import 'package:uuid/uuid.dart';
 
+import 'example_envs.dart';
+
 // sometimes websockets connection is dropped
 // it is better to delegate connection restoration strategy for the application, that uses this library
 const webSocketsTestRetries = 2;
 
 void main() async {
-  // Run commands below in your terminal to generate keys for Alice and Bob:
-  // openssl ecparam -name prime256v1 -genkey -noout -out example/keys/alice_private_key.pem
-  // openssl ecparam -name prime256v1 -genkey -noout -out example/keys/bob_private_key.pem
-  // OR
-  // set environment variables TEST_MEDIATOR_DID, TEST_MEDIATOR_WITH_ACL_DID, TEST_ALICE_PRIVATE_KEY_PEM, and TEST_BOB_PRIVATE_KEY_PEM
-
-  // Create and run a DIDComm mediator, for instance https://github.com/affinidi/affinidi-tdk-rs/tree/main/crates/affinidi-messaging/affinidi-messaging-mediator or with https://portal.affinidi.com.
-  // Copy its DID Document URL into example/mediator/mediator_did.txt.
-  // For TEST_MEDIATOR_WITH_ACL_DID use a mediator with ACL explicit allow.
-
-  const mediatorDidPath = './example/mediator/mediator_did.txt';
-  const mediatorWithAclDidPath = './example/mediator/mediator_with_acl_did.txt';
-  const alicePrivateKeyPath = './example/keys/alice_private_key.pem';
-  const bobPrivateKeyPath = './example/keys/bob_private_key.pem';
-
-  await writeEnvironmentVariableToFileIfNeed(
-    'TEST_MEDIATOR_DID',
-    mediatorDidPath,
-  );
-
-  await writeEnvironmentVariableToFileIfNeed(
-    'TEST_MEDIATOR_WITH_ACL_DID',
-    mediatorWithAclDidPath,
-  );
-
-  await writeEnvironmentVariableToFileIfNeed(
-    'TEST_ALICE_PRIVATE_KEY_PEM',
-    alicePrivateKeyPath,
-    decodeBase64: true,
-  );
-
-  await writeEnvironmentVariableToFileIfNeed(
-    'TEST_BOB_PRIVATE_KEY_PEM',
-    bobPrivateKeyPath,
-    decodeBase64: true,
-  );
+  configureTestFiles();
 
   group('Mediator Integration Test', () {
     late PersistentWallet aliceWallet;
